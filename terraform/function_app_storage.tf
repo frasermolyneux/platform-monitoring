69e04b6be732799eec9b69ecf5874b4d8a82e0ce
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-21 20:01:55 +0000

    Migrate monitoring-func

---
+++ terraform/function_app_storage.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "function_app_storage" {
+  for_each = toset(var.locations)
+
+  name                = "safn${random_id.environment_location_id[each.value].hex}"
+  resource_group_name = azurerm_resource_group.rg[each.value].name
+  location            = azurerm_resource_group.rg[each.value].location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
+
+  local_user_enabled        = true
+  shared_access_key_enabled = true
+
+  tags = var.tags
+}
