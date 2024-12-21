ef2f1f65bcfa8f8f9f22dea9cc00dc390db2a906
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-21 19:22:34 +0000

    Fix name

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "ai" {
-  name                = "ai-platform-monitoring-${var.environment}-${each.value}-${var.instance}"
+  name                = "ai-platform-monitoring-${var.environment}-${var.location}-${var.instance}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   workspace_id        = "/subscriptions/${var.log_analytics_subscription_id}/resourceGroups/${var.log_analytics_resource_group_name}/providers/Microsoft.OperationalInsights/workspaces/${var.log_analytics_workspace_name}"
