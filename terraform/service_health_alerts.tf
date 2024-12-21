6145b33c6e1238a80131583f13bc7e633f07e679
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-21 21:56:35 +0000

    Update name and scope

--- terraform/service_health_alerts.tf
+++ terraform/service_health_alerts.tf
@@ -1,12 +1,12 @@
 resource "azurerm_monitor_activity_log_alert" "subscription_service_health_alerts" {
   for_each = { for each in var.subscriptions : each.name => each }
 
-  name = "platform-${each.key} - service health"
+  name = "platform-${each.key} - ${var.environment} - servicehealth"
 
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   location            = var.locations[0]
 
-  scopes      = [each.value.subscription_id]
+  scopes      = [data.azurerm_subscription.subscriptions[each.key].id]
   description = "Platform service health alert for ${each.key} subscription"
 
   criteria {
