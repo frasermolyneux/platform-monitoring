fc268283135f8035d20c4c9a1afe3f15d0794397
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-22 21:08:19 +0000

    Update action short names

--- terraform/monitor_action_groups.tf
+++ terraform/monitor_action_groups.tf
@@ -1,7 +1,7 @@
 resource "azurerm_monitor_action_group" "critical" {
   name                = "p0-critical-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
-  short_name          = "p0action"
+  short_name          = "p0action${var.environment}"
 
   sms_receiver {
     name         = "sms"
@@ -23,7 +23,7 @@ resource "azurerm_monitor_action_group" "critical" {
 resource "azurerm_monitor_action_group" "high" {
   name                = "p1-high-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
-  short_name          = "p1action"
+  short_name          = "p1action${var.environment}"
 
   sms_receiver {
     name         = "sms"
@@ -45,7 +45,7 @@ resource "azurerm_monitor_action_group" "high" {
 resource "azurerm_monitor_action_group" "moderate" {
   name                = "p2-moderate-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
-  short_name          = "p2action"
+  short_name          = "p2action${var.environment}"
 
   email_receiver {
     name          = "email"
@@ -56,7 +56,7 @@ resource "azurerm_monitor_action_group" "moderate" {
 resource "azurerm_monitor_action_group" "low" {
   name                = "p3-low-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
-  short_name          = "p3action"
+  short_name          = "p3action${var.environment}"
 
   email_receiver {
     name          = "email"
@@ -67,7 +67,7 @@ resource "azurerm_monitor_action_group" "low" {
 resource "azurerm_monitor_action_group" "informational" {
   name                = "p4-informational-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
-  short_name          = "p4action"
+  short_name          = "p4action${var.environment}"
 
   email_receiver {
     name          = "email"
