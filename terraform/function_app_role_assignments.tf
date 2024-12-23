7dd8fb61da7e66d9e5207d5d6766730104dd7ce3
Fraser Molyneux <fmolyneux@microsoft.com>
2024-12-23 14:21:38 +0000

    Removing MI to storage

--- terraform/function_app_role_assignments.tf
+++ terraform/function_app_role_assignments.tf
@@ -1,12 +1,4 @@
-resource "azurerm_role_assignment" "app-to-storage" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_storage_account.function_app_storage[each.value].id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_linux_function_app.app[each.value].identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "app-to-keyvault" {
+resource "azurerm_role_assignment" "app_to_keyvault" {
   for_each = toset(var.locations)
 
   scope                = azurerm_key_vault.kv.id
