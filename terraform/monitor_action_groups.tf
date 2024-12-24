5a9857d9947627ddfd328c96488ad798a5490834
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-24 10:14:35 +0000

    Update monitor action groups

--- terraform/monitor_action_groups.tf
+++ terraform/monitor_action_groups.tf
@@ -3,20 +3,32 @@ resource "azurerm_monitor_action_group" "critical" {
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   short_name          = "p0action${var.environment}"
 
-  sms_receiver {
-    name         = "sms"
-    country_code = "44"
-    phone_number = azurerm_key_vault_secret.alert_phone.value
+  dynamic "sms_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
+
+    content {
+      name         = "sms"
+      country_code = "44"
+      phone_number = azurerm_key_vault_secret.alert_phone.value
+    }
   }
 
-  email_receiver {
-    name          = "email"
-    email_address = azurerm_key_vault_secret.alert_email.value
+  dynamic "email_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
+
+    content {
+      name          = "email"
+      email_address = azurerm_key_vault_secret.alert_email.value
+    }
   }
 
-  azure_app_push_receiver {
-    name          = "push"
-    email_address = azurerm_key_vault_secret.alert_email.value
+  dynamic "azure_app_push_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
+
+    content {
+      name          = "push"
+      email_address = azurerm_key_vault_secret.alert_email.value
+    }
   }
 }
 
@@ -25,20 +37,22 @@ resource "azurerm_monitor_action_group" "high" {
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   short_name          = "p1action${var.environment}"
 
-  sms_receiver {
-    name         = "sms"
-    country_code = "44"
-    phone_number = azurerm_key_vault_secret.alert_phone.value
+  dynamic "email_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
+
+    content {
+      name          = "email"
+      email_address = azurerm_key_vault_secret.alert_email.value
+    }
   }
 
-  email_receiver {
-    name          = "email"
-    email_address = azurerm_key_vault_secret.alert_email.value
-  }
+  dynamic "azure_app_push_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
 
-  azure_app_push_receiver {
-    name          = "push"
-    email_address = azurerm_key_vault_secret.alert_email.value
+    content {
+      name          = "push"
+      email_address = azurerm_key_vault_secret.alert_email.value
+    }
   }
 }
 
@@ -47,9 +61,13 @@ resource "azurerm_monitor_action_group" "moderate" {
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   short_name          = "p2action${var.environment}"
 
-  email_receiver {
-    name          = "email"
-    email_address = azurerm_key_vault_secret.alert_email.value
+  dynamic "email_receiver" {
+    for_each = var.environment == "prd" ? [1] : []
+
+    content {
+      name          = "email"
+      email_address = azurerm_key_vault_secret.alert_email.value
+    }
   }
 }
 
@@ -57,20 +75,10 @@ resource "azurerm_monitor_action_group" "low" {
   name                = "p3-low-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   short_name          = "p3action${var.environment}"
-
-  email_receiver {
-    name          = "email"
-    email_address = azurerm_key_vault_secret.alert_email.value
-  }
 }
 
 resource "azurerm_monitor_action_group" "informational" {
   name                = "p4-informational-alerts-${var.environment}"
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
   short_name          = "p4action${var.environment}"
-
-  email_receiver {
-    name          = "email"
-    email_address = azurerm_key_vault_secret.alert_email.value
-  }
 }
