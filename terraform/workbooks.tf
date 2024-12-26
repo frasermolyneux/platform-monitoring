9ecb50652ff82b7c3861223320fd1bfdc0c85ca7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-26 15:30:42 +0000

    Adding workbooks

---
+++ terraform/workbooks.tf
@@ -0,0 +1,21 @@
+locals {
+  workbook_templates = [for f in fileset("workbooks", "*.json") : {
+    workbook_name = replace(f, ".json", "")
+    data_json     = jsondecode(file("workbooks/${f}"))
+  }]
+}
+
+resource "azurerm_application_insights_workbook" "workbook" {
+  for_each = { for each in local.workbook_templates : each.workbook_name => each }
+
+  name = "platform-monitoring-${each.key}-${var.environment}"
+
+  resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
+  location            = azurerm_resource_group.rg[var.locations[0]].name
+
+  display_name = "platform-monitoring-${each.key}-${var.environment}"
+
+  data_json = each.value.data_json
+
+  tags = var.tags
+}
