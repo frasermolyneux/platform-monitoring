11157499d8ad8f5be6fc7fee14767dcc719e3ce2
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-27 22:16:10 +0000

    Set log analytics retention period to a fixed value of 30 days

--- terraform/log_analytics.tf
+++ terraform/log_analytics.tf
@@ -4,7 +4,7 @@ resource "azurerm_log_analytics_workspace" "law" {
   resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
 
   sku               = "PerGB2018"
-  retention_in_days = var.environment == "dev" ? 7 : 30
+  retention_in_days = 30
 
   tags = var.tags
 }
