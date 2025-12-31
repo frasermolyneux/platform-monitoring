ae0dd2c74ce3727735f01050b5140572bd0f950c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-31 13:21:50 +0000

    Add subscription_id to monitor action groups output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -25,26 +25,31 @@ output "monitor_action_groups" {
       id                  = azurerm_monitor_action_group.critical.id
       name                = azurerm_monitor_action_group.critical.name
       resource_group_name = azurerm_monitor_action_group.critical.resource_group_name
+      subscription_id     = var.subscription_id
     }
     high = {
       id                  = azurerm_monitor_action_group.high.id
       name                = azurerm_monitor_action_group.high.name
       resource_group_name = azurerm_monitor_action_group.high.resource_group_name
+      subscription_id     = var.subscription_id
     }
     moderate = {
       id                  = azurerm_monitor_action_group.moderate.id
       name                = azurerm_monitor_action_group.moderate.name
       resource_group_name = azurerm_monitor_action_group.moderate.resource_group_name
+      subscription_id     = var.subscription_id
     }
     low = {
       id                  = azurerm_monitor_action_group.low.id
       name                = azurerm_monitor_action_group.low.name
       resource_group_name = azurerm_monitor_action_group.low.resource_group_name
+      subscription_id     = var.subscription_id
     }
     informational = {
       id                  = azurerm_monitor_action_group.informational.id
       name                = azurerm_monitor_action_group.informational.name
       resource_group_name = azurerm_monitor_action_group.informational.resource_group_name
+      subscription_id     = var.subscription_id
     }
   }
 }
