b444363a7a3fb3f3a15d7134e39c66d1d3d4a2e7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-02-26 19:03:09 +0000

    fix: correct indexing and count conditions for management group resources in policy assignment

--- terraform/policy_assignment_activity_log.tf
+++ terraform/policy_assignment_activity_log.tf
@@ -1,18 +1,20 @@
 data "azurerm_management_group" "alz" {
-  name = "alz"
+  count = var.environment == "prd" ? 1 : 0
+  name  = "alz"
 }
 
 import {
-  to = azurerm_management_group_policy_assignment.deploy_az_activity_log
+  to = azurerm_management_group_policy_assignment.deploy_az_activity_log[0]
   id = "/providers/Microsoft.Management/managementGroups/alz/providers/Microsoft.Authorization/policyAssignments/Deploy-AzActivity-Log"
 }
 
 resource "azurerm_management_group_policy_assignment" "deploy_az_activity_log" {
+  count                = var.environment == "prd" ? 1 : 0
   name                 = "Deploy-AzActivity-Log"
   display_name         = "Deploy Diagnostic Settings for Activity Log to Log Analytics workspace"
   description          = "Ensures that Activity Log Diagnostics settings are set to push logs into Log Analytics workspace."
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/2465583e-4e78-4c15-b6be-a36cbc7c8b0f"
-  management_group_id  = data.azurerm_management_group.alz.id
+  management_group_id  = data.azurerm_management_group.alz[0].id
   enforce              = true
 
   identity {
@@ -32,7 +34,8 @@ resource "azurerm_management_group_policy_assignment" "deploy_az_activity_log" {
 }
 
 resource "azurerm_role_assignment" "activity_log_policy_owner" {
-  scope                = data.azurerm_management_group.alz.id
+  count                = var.environment == "prd" ? 1 : 0
+  scope                = data.azurerm_management_group.alz[0].id
   role_definition_name = "Owner"
-  principal_id         = azurerm_management_group_policy_assignment.deploy_az_activity_log.identity[0].principal_id
+  principal_id         = azurerm_management_group_policy_assignment.deploy_az_activity_log[0].identity[0].principal_id
 }
